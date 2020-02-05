5d25b00eafca4757daaf51a1e8e574a1d0dd454e
eleilsonn <eleilsomaciel@gmail.com>
2020-02-05 10:07:49 -0300

    Update 1

--- Utils.js
+++ Utils.js
@@ -1,47 +1,91 @@
 'use-strict'
-const utils = () =>{
-
-    const resetForm = (form,selectpicker)=>{
-        form = form.replace('#','');
-        form = '#'+form;
-
-        $(form+' input*').val('');
-        $(form+' input[type="checkbox"]').prop('checked', false);
-        $(form+' select option[value=""]').prop('selected', true);        
-
-        if( typeof selectpicker !== 'undefined' && selectpicker)
-            $(form+' select').selectpicker('refresh');        
+function formatTypeDate(date){
+    date = date.split('T');
+    date = date[0];   
+    
+    date = date.split('-');
+    if(date.length > 2){
+        if(date[0].length > 2){
+            date = date[0]+'-'+date[1]+'-'+date[2];
+        }else{
+            date = date[2]+'-'+date[1]+'-'+date[0];           
+        }
+    }else{
+        date= date[0];
     }
     
-    const getObjectForm = (form, inputs)=>{
+    date = date.split('/');    
+    if(date.length > 1){
+        if(date[0].length > 2){
+            date = date[0]+'-'+date[1]+'-'+date[2];
+        }else{
+            date = date[2]+'-'+date[1]+'-'+date[0];
+        }
+    }else{
+        date = date[0];
+    }  
+    
+    return date.replace(/\ /g,'');
+}
+const utils = () =>{
+    var forms = {}
+    const setForm = (idForm,form)=>{
+        idForm = idForm.replace('#','');
+        idForm = idForm.replace(/\-/g,'_');
+        forms[idForm] = form;
+    }
+    const resetForm = (idForm,selectpicker)=>{
+        idForm = idForm.replace('#','');
+        idForm = '#'+idForm;
+
+        $(idForm+' input*').val('');
+        $(idForm+' input[type="checkbox"]').prop('checked', false);
+        $(idForm+' select option[value=""]').prop('selected', true);        
+
+        if( typeof selectpicker !== 'undefined' && selectpicker)
+            $(idForm+' select').selectpicker('refresh');        
+    }
+    
+    const getObjectForm = (idForm)=>{
         let result = new Object();
-        form = form.replace('#','');
-        form ='#'+form;
-        
+        idForm = idForm.replace('#','');
+        idForm ='#'+idForm;
+        let form = idForm.replace(/\-/g,'_').replace('#','');
+        let inputs = forms[form];
+       
        inputs.forEach((item)=>{
          switch(item.type){
              case 'input':
-               result[item.name] = {value:$(form+' '+item.type+'[name="'+item.name+'"]').val()}
+               result[item.name] = {
+                   value:$(idForm+' input[name="'+item.name+'"]').val() != ''?$(idForm+' input[name="'+item.name+'"]').val():null}
                break;
+             case 'date':
+                result[item.name] = {
+                    value:$(idForm+' input[name="'+item.name+'"]').val() != ''?$(idForm+' input[name="'+item.name+'"]').val():null}
+                break;
              case 'select':
                 result[item.name] = {
-                    value:$(form+' select[name="'+item.name+'"]').val(),
-                    text:$(form+' select[name="'+item.name+'"] option:selected').text()
+                    value:$(idForm+' select[name="'+item.name+'"]').val() != ''?$(idForm+' select[name="'+item.name+'"]').val():null,
+                    text:$(idForm+' select[name="'+item.name+'"]').val() != ''?$(idForm+' select[name="'+item.name+'"] option:selected').text():null
                 }       
                 break;
             case 'textarea':
-                result[item.name] = {value:$(form+' textarea[name="'+item.name+'"]').val()}
+                result[item.name] = {
+                    value:$(idForm+' textarea[name="'+item.name+'"]').val() != ''? $(idForm+' textarea[name="'+item.name+'"]').val():null
+                }
                 break;
             case 'checkbox':
-                result[item.name] = {value:$(form+' input[name="'+item.name+'"]').prop('checked')}
+                result[item.name] = {
+                    value:$(idForm+' input[name="'+item.name+'"]').prop('checked')
+                }
                 break;
             case 'radio':
-                result[item.name] = {value: $(form+' input[name="'+item.name+'"]:checked').val()}
+                result[item.name] = {value: $(idForm+' input[name="'+item.name+'"]:checked').val()}
                 break;
             default:
                 result['undefined_type'] = {
-                    value:$(form+' '+item.type+'[name="'+item.name+'"]').val(),
-                    text:$(form+' '+item.type+'[name="'+item.name+'"]').text()
+                    value:$(idForm+' '+item.type+'[name="'+item.name+'"]').val(),
+                    text:$(idForm+' '+item.type+'[name="'+item.name+'"]').text()
                 }  
                break;
          }
@@ -49,13 +93,56 @@ const utils = () =>{
       return result
     }
 
-    const setValuesInInputs = (form,inputs,values)=>{
+    const setValuesInInputs = (idForm,especification,obj,selectpicker)=>{
         
+        let form = idForm.replace(/\-/g,'_').replace('#');
+        let inputs = forms[form];
+
+        idForm = idForm.replace(/\#/g,'');
+        idForm ='#'+idForm;
+
+        let keysEspecification = Object.keys(especification);       
+
+        keysEspecification.forEach((key)=>{
+            inputs.forEach((input)=>{
+                if(key == input.name){
+                   
+                    switch(input.type){
+                        case 'input':
+                            $(idForm+' input[name="'+input.name+'"]').val(obj[especification[key]]);
+                        break;
+                        case 'date':
+                            let date = formatTypeDate(obj[especification[key]]);
+                            $(idForm+' input[name="'+input.name+'"]').val(date);
+                            break;
+                        case 'select':
+                            $(idForm+' select[name="'+input.name+'"] option[value="'+obj[especification[key]]+'"]').prop('selected', true);
+                            if(typeof selectpicker !== 'undefined' && selectpicker)
+                                $(idForm+' select').selectpicker('refresh');      
+                            break;
+                        case 'textarea':
+                            $(idForm+' textarea[name="'+input.name+'"]').val(obj[especification[key]]);
+                            break;
+                        case 'checkbox':
+                           $(idForm+' input[name="'+input.name+'"]').prop('checked',obj[especification[key]]);
+                            break;
+                        case 'radio':
+                            $(idForm+' input[name="'+input.name+'"][value="'+obj[especification[key]]+'"]').prop('checked',true);
+                            break;
+                        default:
+                            console.exception('Type undefined!',input); 
+                        break;
+                    }
+                }
+            })
+        })
     }
 
     return {
         resetForm,
-        getObjectForm
+        getObjectForm,
+        setValuesInInputs,
+        setForm
     }
 }
 
--- demo/index.js
+++ demo/index.js
@@ -1,41 +1,42 @@
 //VARIAVEL QUE CONTEM TODAS AS FUNÇÕES DE UTILS
 const methodUtils = utils();
-//VARIAVEL PARA ARMAZENAR FORMULATIOS
-const forms = {};
 
 $(document).ready(function(){
-    //DEFININDO OS INPUTS DO FORMULARIO
+    //DEFININDO OS ESPECIFICAÇÕES DO FORMULARIO
     //name = Nome da tag
     //type = tipo de tag (input, select...)
-    forms['formulario'] = [
+    let formulario = [
             {name:'nome',type:'input'},
             {name:'email',type:'input'},
             {name:'telefone',type:'input'},
             {name:'cidade',type:'input'},
             {name:'uf',type:'select'},
-            {name:'nascimento',type:'input'},
+            {name:'nascimento',type:'date'},
             {name:'sexo',type:'radio'},
             {name:'notificacao',type:'checkbox'},
             {name:'alertas',type:'checkbox'}
         ]
+    //PARAMETROS(ID_DO_FORMULARIO,ESPECIFICAÇÕES_DO_FORMULARIO)   
+    methodUtils.setForm('form',formulario);
+   
     //DEFININDO O SELECT PICKER 
     $('select[name="uf"]').selectpicker();
 
     //AÇÕES DO BUTTON MONTAR
     $('button[name="montar"]').click(function(){
-        //METODO QUE MONTA OBJETO DOS INPUT`S DO FORMULARIO, PARAMETROS(ID_DO_FORMULARIO, ESPECIFICAÇAO_DO_FOMULARIO)
-        let obj = methodUtils.getObjectForm('form',forms.formulario);
+        //METODO QUE MONTA OBJETO DOS INPUT`S DO FORMULARIO, PARAMETROS(ID_DO_FORMULARIO)
+        let obj = methodUtils.getObjectForm('form');
         // 
         $('#resultado').empty();         
-        let itens = '<li>Nome: '+obj.nome.value+'</li>'+
-                    '<li>Email: '+obj.email.value+'</li>'+
-                    '<li>Telefone: '+obj.telefone.value+'</li>'+
-                    '<li>Cidade: '+obj.cidade.value+'</li>'+
-                    '<li>Estado: '+obj.uf.value+'</li>'+
-                    '<li>Data de Nascimento: '+obj.nascimento.value+'</li>'+
-                    '<li>Sexo: '+obj.sexo.value+'</li>'+
-                    '<li>Notificação: '+obj.notificacao.value+'</li>'+
-                    '<li>Alertas: '+obj.alertas.value+'</li>';
+        let itens = '<li><strong>Nome:</strong> '+obj.nome.value+'</li>'+
+                    '<li><strong>Email:</strong> '+obj.email.value+'</li>'+
+                    '<li><strong>Telefone:</strong> '+obj.telefone.value+'</li>'+
+                    '<li><strong>Cidade:</strong> '+obj.cidade.value+'</li>'+
+                    '<li><strong>UF:</strong> '+obj.uf.value+' <strong>Estado:</strong> '+obj.uf.text+'</li>'+
+                    '<li><strong>Data de Nascimento:</strong> '+obj.nascimento.value+'</li>'+
+                    '<li><strong>Sexo:</strong> '+obj.sexo.value+'</li>'+
+                    '<li><strong>Notificação:</strong> '+obj.notificacao.value+'</li>'+
+                    '<li><strong>Alertas:</strong> '+obj.alertas.value+'</li>';
         
         $('#resultado').append(itens);
     })
@@ -45,6 +46,36 @@ $(document).ready(function(){
         //true => informa que formulario utiliza selectpicker, caso não use basta remover ou colocar false
         methodUtils.resetForm('form',true);
         
+    })
+    //AÇÃO PARA BUTTON PREENCHER
+    $('button[name="preencher"]').click(function(){
+        //OBJETO PARA PREENCHER
+        let obj = {
+            nome_usuario:"José Francisco",
+            email:"email@email.com",
+            telefone_user:"99 9 9999-9999",
+            cidade:"Teresina",
+            uf:'PI',
+            data_de_nascimento:'2012/12/12 T09090',
+            sexo:'masculino',
+            notificacao:false,
+            receber_alertas:true
+        }//FIM DO OBJETO
+
+        //ESPECIFICAÇÕES NECESSÁRIA PARA PREENCHER COMBO nome das variaveis(em especification) = name do input, e valor da variavel = nome da variavel do obj
+        let especification = {
+            nome:'nome_usuario',
+            email:'email',
+            telefone:'telefone_user',
+            cidade:'cidade',
+            uf:'uf',
+            nascimento:'data_de_nascimento',
+            sexo:'sexo',
+            notificacao:'notificacao',
+            alertas:'receber_alertas'
+        }
+        //METODO PREENCHER FORMULARIO, PARAMETROS(ID_DO_FORMULARIO, ESPECIFICAÇOES_DO_OBJETO, OBJETO, SELECTPICKER)
+        methodUtils.setValuesInInputs('form',especification,obj,true);
     })
 
 })
